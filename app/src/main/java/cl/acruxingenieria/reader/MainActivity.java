8470e477b19c1e9fd4833a43f56cfca7f29e6e0e
Sebastian <sebastian.mofre@gmail.com>
2019-02-05 14:29:40 -0300

    Initial commit

---
+++ app/src/main/java/cl/acruxingenieria/reader/MainActivity.java
@@ -0,0 +1,115 @@
+package cl.acruxingenieria.reader;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity{
+
+    private NfcAdapter mNfcAdapter;
+    ArrayList<String> ID = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        configureTextViewData();
+        configureButtonClear();
+        configureNFCAdapter();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        IntentFilter ndefDetected = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        IntentFilter techDetected = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+        IntentFilter[] nfcIntentFilter = new IntentFilter[]{techDetected,tagDetected,ndefDetected};
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        if(mNfcAdapter!= null)
+            mNfcAdapter.enableForegroundDispatch(this, pendingIntent, nfcIntentFilter, null);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mNfcAdapter!= null)
+            mNfcAdapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+        if(tag != null) {
+            String id = bytesToHexString(tag.getId());
+            if (id != null){
+                ID.add(id);
+                configureTextViewData();
+            }
+
+
+        }
+    }
+
+    private void configureTextViewData() {
+        TextView tv_data = (TextView) findViewById(R.id.textViewData);
+        tv_data.setMovementMethod(new ScrollingMovementMethod());
+
+        tv_data.setText("");
+        for (int i=0; i<ID.size();i++){
+            tv_data.append(ID.get(i));
+            tv_data.append("\n");
+        }
+    }
+
+    private void configureButtonClear() {
+        Button bt = (Button) findViewById(R.id.buttonClear);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ID = new ArrayList<>();
+                configureTextViewData();
+
+            }
+        });
+
+    }
+
+    private void configureNFCAdapter(){
+        //Init NFC Adapter
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+    }
+
+    private String bytesToHexString(byte[] src) {
+        StringBuilder stringBuilder = new StringBuilder("");
+        if (src == null || src.length <= 0) {
+            return null;
+        }
+
+        char[] buffer = new char[2];
+        for (int i = 0; i < src.length; i++) {
+            buffer[0] = Character.forDigit((src[i] >>> 4) & 0x0F, 16);
+            buffer[1] = Character.forDigit(src[i] & 0x0F, 16);
+            System.out.println(buffer);
+            stringBuilder.append(buffer);
+        }
+
+        return stringBuilder.toString().toUpperCase();
+    }
+
+}
